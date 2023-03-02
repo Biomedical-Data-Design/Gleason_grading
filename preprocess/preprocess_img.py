d42e71d7c5226f6b7eef254d885eb873d6c96307
atandri1 <112509908+atandri1@users.noreply.github.com>
2023-03-01 19:56:26 -0500

    Add files via upload

--- preprocess/preprocess_img.py
+++ preprocess/preprocess_img.py
@@ -7,53 +7,52 @@ from PIL import Image
 #from skimage import io
 import numpy as np
 from patchify import patchify 
-#import imageio
+import imageio
 
-Image.MAX_IMAGE_PIXELS = 100000000000 
+Image.MAX_IMAGE_PIXELS = 10000000000000
 os.environ['OPENCV_IO_MAX_IMAGE_PIXELS']= pow(2,40).__str__()
 import cv2
 ##### get path
 import argparse
 from argparse import ArgumentParser
-import openslide
 
 parser = ArgumentParser(description="Preprocessed File")
 parser.print_help()
 parser.add_argument("--i", dest="filename", required=False,
-    help="input original image", metavar="FILE", default = 'Subset1_Train_1.tiff')
+    help="input original image", metavar="FILE", default = 'Subset1_Train_96.tiff')
 parser.add_argument('--annotation', dest="annotation_dir", required=False,
-    help="annotation directory", default = 'Subset1_Train_1')
+    help="annotation directory", default = 'Subset1_Train_96')
 parser.add_argument('--output', dest="output_dir", required=False,
-    help="output directory",default = 'Subset1_Train_1')
+    help="output directory",default = 'Subset1_Train_96')
 args = parser.parse_args()
 
 
 print(args.filename,args.annotation_dir,args.output_dir)
 
-
-image = os.path.join("/home/yzhao155/data-acharl15/gleason_grading_yujie/"+ args.filename)
-ann_dir = os.path.join('/home/yzhao155/data-acharl15/gleason_grading/Subset1_Train_annotation/'+ args.annotation_dir+"/")
-output = os.path.join("/home/yzhao155/data-acharl15/gleason_grading_yujie/patch/"+args.output_dir+"/")
+#determines where images, annotations, and patch masks are saved
+image = os.path.join("/Users/anany/Desktop/gleason-grading/goodimg/"+ args.filename)
+ann_dir = os.path.join("/Users/anany/Desktop/gleason-grading/goodimg/"+args.filename[0:7]+"_Train_annotation/"+ args.annotation_dir+"/")
+output = os.path.join("/Users/anany/Desktop/gleason-grading/patch/"+args.output_dir+"/")
 
 print(image, ann_dir, output)
 
+#creates a list of all the annotations for the particular image
 annotation_image = []
 for i in os.listdir(ann_dir):
     if i.endswith('tif'):
         annotation_image.append(os.path.join(ann_dir,i))
 print(annotation_image)
 
-# transfer BGR format to gray format
+#verifies image path
 if os.path.exists(image):
     print("path exists")
 else:
     print("no pathway")
 
+# transfer BGR format for image to gray format
 print(type(image))
-img = cv2.imread(str("/home/yzhao155/data-acharl15/gleason_grading_yujie/"+ args.filename),1)
-#img=openslide.OpenSlide(str("/home/yzhao155/data-acharl15/gleason_grading_yujie/"+ args.filename))
-#print(img.dimensions)
-#print(type(img))
+img = cv2.imread(str("/Users/anany/Desktop/gleason-grading/goodimg/"+ args.filename),1)
+print(img.shape)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # cv2.threshold(): choose a threshold (thresh_ostu choose a value between two peaks), https://docs.opencv.org/4.x/d7/d4d/tutorial_py_thresholding.html
@@ -61,12 +60,13 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 #https://blog.csdn.net/lcalqf/article/details/71170171
 otsu,thresh = cv2.threshold(gray,1,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 
-#how many patch can be make
+#determine how many patches can be made from each image
 row_num = int(thresh.shape[0]/256)
 col_num = int(thresh.shape[1]/256)
 
 print(row_num, col_num)
-# cut edge
+
+# Determines places to crop picture so it is divisible by 256
 row_left_top = int((thresh.shape[0]-row_num*256)/2)
 row_left_bottom = int((thresh.shape[0]-row_num*256)-row_left_top)
 col_left_top = int((thresh.shape[1]-col_num*256)/2)
@@ -75,6 +75,7 @@ col_left_bottom = int((thresh.shape[1]-col_num*256)-col_left_top)
 patch_mask = np.zeros([row_num,col_num],dtype=int)
 patch_mask_annotation = np.zeros([5,row_num,col_num],dtype=int)
 
+#cropping the image and creating the patches
 patches_img = patchify(thresh[int(row_left_top):int(thresh.shape[0]-row_left_bottom),
                            int(col_left_top):int(thresh.shape[1]-col_left_bottom)], (256, 256), step=256)
 
@@ -86,7 +87,7 @@ for i in annotation_image:
     order_annotation.append(gleason_class)
     annotation_mask.append(img_annotate)
 
-
+# creates a dictionary of the number of pixels in each 256x256 patch in each category
 def align_annotate(sinlgle_patch_index,annotation_mask, order_annotation):
     annotate_dict = {'Normal':0,'Stroma': 0,'G3': 0,'G4': 0,'G5': 0}
     for i in range(len(annotation_mask)):
@@ -99,6 +100,7 @@ def align_annotate(sinlgle_patch_index,annotation_mask, order_annotation):
             annotate_dict[gleason_class] = 1
     return annotate_dict
 
+# delete empty space and unannotated areas, aka patches with <1% annotation pixels
 count=0
 for i in range(patches_img.shape[0]):
     for j in range(patches_img.shape[1]):
@@ -109,24 +111,25 @@ for i in range(patches_img.shape[0]):
         if (non_zero / (256*256)) > 0.01:
             row_start = int(row_left_top+i*256)
             col_start = int(col_left_top+j*256)
-            # index on the original 
+            # index on the original
             single_patch=[row_start, int(row_start+256),col_start,int(col_start+256)]
             annotation = align_annotate(single_patch, annotation_mask,order_annotation)
             if np.sum(list(annotation.values())) > 0:
-                patch_mask[i,j]=1
+                patch_mask[i,j]=1  #if any annotation value is present, add to overall patch mask
                 for k in range(len(annotation.keys())):
                     #print("have annotation")
                     patch_mask_annotation[k][i,j]=list(annotation.values())[k]
 
-
+#create black and white classification masks
 patch_mask_output = cv2.convertScaleAbs(patch_mask, alpha=(255.0))
 cv2.imwrite(str(output+"patch_mask.jpg"), patch_mask_output)
 
 for i in range(5):
     patch_mask_annotation[i]= cv2.convertScaleAbs(patch_mask_annotation[i], alpha=(255.0))
 
-cv2.imwrite(str("/home/yzhao155/data-acharl15/gleason_grading_yujie/patch/"+args.output_dir+"/patch_mask_Normal.jpg"), patch_mask_annotation[0])
-cv2.imwrite(str("/home/yzhao155/data-acharl15/gleason_grading_yujie/patch/"+args.output_dir+"/patch_mask_Stroma.jpg"), patch_mask_annotation[1])
-cv2.imwrite(str("/home/yzhao155/data-acharl15/gleason_grading_yujie/patch/"+args.output_dir+"/patch_mask_G3.jpg"), patch_mask_annotation[2])
-cv2.imwrite(str("/home/yzhao155/data-acharl15/gleason_grading_yujie/patch/"+args.output_dir+"/patch_mask_G4.jpg"), patch_mask_annotation[3])
-cv2.imwrite(str("/home/yzhao155/data-acharl15/gleason_grading_yujie/patch/"+args.output_dir+"/patch_mask_G5.jpg"), patch_mask_annotation[4])
+#save each of the classifications for the patches
+cv2.imwrite(str("/Users/anany/Desktop/gleason-grading/patch/"+args.output_dir+"/patch_mask_Normal.jpg"), patch_mask_annotation[0])
+cv2.imwrite(str("/Users/anany/Desktop/gleason-grading/patch/"+args.output_dir+"/patch_mask_Stroma.jpg"), patch_mask_annotation[1])
+cv2.imwrite(str("/Users/anany/Desktop/gleason-grading/patch/"+args.output_dir+"/patch_mask_G3.jpg"), patch_mask_annotation[2])
+cv2.imwrite(str("/Users/anany/Desktop/gleason-grading/patch/"+args.output_dir+"/patch_mask_G4.jpg"), patch_mask_annotation[3])
+cv2.imwrite(str("/Users/anany/Desktop/gleason-grading/patch/"+args.output_dir+"/patch_mask_G5.jpg"), patch_mask_annotation[4])
