0dc749931306abc4e1218a7da75b6edcacc9efae
yzhao155 <yzhao155@login01.rockfish.cluster>
2023-04-09 21:30:21 -0400

    YZ

--- neural_network_training/Model/model.py
+++ neural_network_training/Model/model.py
@@ -11,6 +11,7 @@ class Model_name(nn.Module):
         self.classifier = nn.Sequential(OrderedDict([
             ('fc1',nn.Linear(1024, 512)),
             ('relu',nn.ReLU()),
+            ('dropout',nn.Dropout(p=0.5)),
             ('fc2',nn.Linear(512, 5))
             #('output',nn.Softmax(dim=1))     
         ]))
